948117da387bf9b8d0ec77a460e21f2b9f50573b
dipin112 <pdipinraj18@gmail.com git config --global user.name>
2024-10-23 15:45:59 +0530

    first updation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./ui/navbar/navbar.component";
+import { CardComponent } from "./ui/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
